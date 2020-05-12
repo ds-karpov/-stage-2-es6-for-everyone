534db4ba7351e47ab141427a2b799b6f0c9b8cf6
Denis Karpov <karpov.syned@gmail.com>
2020-05-12 23:38:11 +0300

    feat: add demonstration of fighter info

--- src/javascript/components/fighterPreview.js
+++ src/javascript/components/fighterPreview.js
@@ -7,7 +7,37 @@ export function createFighterPreview(fighter, position) {
     className: `fighter-preview___root ${positionClassName}`,
   });
 
-  // todo: show fighter info (image, name, health, etc.)
+  function createProperty(keyValue) {
+    const nameElement = createElement({ tagName: 'span', className: 'fighter-preview___property' });
+    nameElement.innerText = keyValue.join(': ').replace(/(\w+):/, subStr => subStr.toUpperCase());
+  
+    return nameElement;
+  }
+
+  function createPreviewImage(source) {
+    const attributes = { src: source };
+    const imgElement = createElement({
+      tagName: 'img',
+      className: 'fighter-image___preview',
+      attributes
+    });
+
+    if(position === 'right') {
+      imgElement.style.transform = 'scale(-1, 1)';
+    }
+  
+    return imgElement;
+  }
+
+  if(fighter) {
+    const keyValueArray = Object.entries(fighter);
+    fighterElement.append(createPreviewImage(fighter['source']));
+    const fightPropsContainer = createElement({ tagName: 'div', className: 'fighter-preview___props-box' });
+    keyValueArray
+    .filter(keyValueAll => keyValueAll[0] !== '_id' && keyValueAll[0] !== 'source')
+    .forEach(keyValue => fightPropsContainer.append(createProperty(keyValue)));
+    fighterElement.append(fightPropsContainer);
+  }
 
   return fighterElement;
 }
